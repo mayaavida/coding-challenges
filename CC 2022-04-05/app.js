7bac654b63e916bddf34c1055e4bf9bb219fcdd7
Maya Peters <mayampeters@gmail.com>
2022-04-05 11:57:35 -0500

    More coding challenges from 4.5.22

--- CC 2022-04-05/app.js
+++ CC 2022-04-05/app.js
@@ -21,3 +21,23 @@ function basicOp(operation, value1, value2)
     return value1 / value2;
   }
 }
+
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example:
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+function digitize(str) {
+    return String(str).split('').reverse().map(Number);
+  }
