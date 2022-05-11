d9924ce240549e6d42140c3df5c346e2a3b51a6b
Maya Peters <mayampeters@gmail.com>
2022-05-10 21:32:24 -0500

    coding challenges from May 10, 2022

---
+++ CC 2022-05-10/main.js
@@ -0,0 +1,42 @@
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+
+function solution(a, b){
+    return a.length > b.length ? b + a + b : a + b + a
+  }
+
+//   It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+// Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+// Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell and Lua) or "¥" (Rust).
+
+function bonusTime(salary, bonus) {
+    return bonus ? `£${salary*10}`: `£${salary}`
+  }
+
+//   Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. 
+
+  String.prototype.toAlternatingCase = function () {
+    return this.split('').map(char => char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()).join('')
+  }
+
+
+//   Given a number n, return the number of positive odd numbers below n, EASY!
+
+// Examples (Input -> Output)
+// * 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// * 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13
+
+function oddCount(n){
+    return Math.floor(n/2)
+  }
