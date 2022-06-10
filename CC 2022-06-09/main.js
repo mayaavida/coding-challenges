201b30952deadd5bcfcf79444119a3b4229f3db2
Maya Peters <mayampeters@gmail.com>
2022-06-09 21:26:20 -0500

    more coding challenges and solutions from June

---
+++ CC 2022-06-09/main.js
@@ -0,0 +1,30 @@
+// The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
+
+// You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
+
+// For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
+
+// All inputs will be integers. Please return an integer. Round down.
+
+function dutyFree(normPrice, discount, hol){
+    let amountSaved = normPrice * discount
+    let needToBuy = Math.floor((hol / amountSaved) * 100)
+    return needToBuy
+  }
+
+//   In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+
+//   false xor false == false // since both are false
+// true xor false == true // exactly one of the two expressions are true
+// false xor true == true // exactly one of the two expressions are true
+// true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
+// Task
+// Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+
+function xor(a, b) {
+    if((a === true && b === false) || (a === false && b === true)){
+      return true
+    }else{
+      return false
+    }
+  }
