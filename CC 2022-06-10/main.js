04d95901882cfb4801c27981d7557d5d33bc75b1
Maya Peters <mayampeters@gmail.com>
2022-06-10 09:21:13 -0500

    coding challenges and solutions from June 10 2022. Record 5 days in a row

---
+++ CC 2022-06-10/main.js
@@ -0,0 +1,25 @@
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+    return a.filter(value => !(b.includes(value)))
+  }
+
+//   Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n){
+    return Number(n.toString().split('').sort((a,b) => b-a).join(''))
+  }
