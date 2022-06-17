3d097e00cfb9619875913a9ccc1782ddc8c0d5e9
Maya Peters <mayampeters@gmail.com>
2022-06-16 22:09:56 -0500

    coding challenges and solutions from June 16 2022

---
+++ CC 2022-06-16/main.js
@@ -0,0 +1,16 @@
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+function remainder(n, m){
+    if(n>m && m !=0){
+      return n%m
+    }else if(m>n && n !=0){
+      return m%n
+    }else if(m == n){
+      return m%n
+    }
+    else{
+      return NaN
+    }
+  }
