ae662ebf8880d23345f9819e2b759fd0bbaae3f0
Maya Peters <mayampeters@gmail.com>
2022-06-29 16:21:10 -0500

    coding challenge from 6.29.22

---
+++ CC 2022-06-29/main.js
@@ -0,0 +1,3 @@
+// Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+
+let apple = x => (x*x) > 1000 ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox.`
